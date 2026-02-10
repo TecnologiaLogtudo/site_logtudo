2968a3e5e3b792106515bc6832f752c350f80038
FelipeLeal92 <fmatheus1992@gmail.com>
2026-02-10 16:05:35 -0300

    fix: configuração final para github pages com caminhos relativos e hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Contato from "./pages/Contato";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
@@ -20,7 +20,7 @@ const App = () => (
       <ContentProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Site_logtudo"}>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/contato" element={<Contato />} />
